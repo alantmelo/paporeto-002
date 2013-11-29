d334289e2f506a7c7447e7ecba660b631dd10b1c
Julio Protzek <julio@startae.com.br>
2013-11-29 11:17:16 -0200

    Category and Article association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131012143332) do
+ActiveRecord::Schema.define(version: 20131123225835) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20131012143332) do
     t.datetime "published_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
+
+  add_index "articles", ["category_id"], name: "index_articles_on_category_id"
 
   create_table "categories", force: true do |t|
     t.string   "name"
