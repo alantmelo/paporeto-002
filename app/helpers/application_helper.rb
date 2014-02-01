54c5bb398fa630ccbcdd3311d4da756995ba51e7
Julio Protzek <julio@startae.com.br>
2014-02-01 20:04:09 -0200

    better login form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,4 @@
 module ApplicationHelper
-  def today
-    localize Date.today, format: :today
-  end
-
   def time_to_read(text)
     time = text.split.size / 180.0
     distance_of_time_in_words(time.minutes)
@@ -11,14 +7,6 @@ module ApplicationHelper
   def nav_status(controller)
     if params[:controller] == controller
       'active'
-    end
-  end
-
-  def maybe_friday
-    if Date.today.friday?
-      'Uhuuu!!!'
-    else
-      'É quase sexta'
     end
   end
 end
