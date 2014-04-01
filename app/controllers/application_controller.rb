eeeed079d3fea131297e7578b082bdde4f24f69c
Julio Protzek <julio@startae.com.br>
2014-04-01 14:10:58 -0300

    fix login layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :layout_for_devise
+
+  def layout_for_devise
+    if devise_controller?
+      'admin'
+    end
+  end
 end
