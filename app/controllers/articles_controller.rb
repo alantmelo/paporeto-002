b92b176ad07073fd8271a3599c3d946e89ce124a
Julio Protzek <julio@startae.com.br>
2014-04-06 11:53:02 -0300

    filter articles by category

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,16 @@
 class ArticlesController < ApplicationController
+  before_action :set_category
+
   def index
-    @articles = Article.all
+    @articles = @category.articles.limit(8)
   end
 
   def show
     @article = Article.find(params[:id])
   end
+
+  private
+    def set_category
+      @category = Category.find(params[:category_id])
+    end
 end
