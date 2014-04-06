8fd0c8e8c0512fe96d71b7d01fc25fb181071d37
Julio Protzek <julio@startae.com.br>
2014-04-06 11:24:24 -0300

    list categories no navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_categories
+
   layout :layout_for_devise
 
-  def layout_for_devise
-    if devise_controller?
-      'admin'
+  private
+    def layout_for_devise
+      if devise_controller?
+        'admin'
+      end
     end
-  end
+
+    def set_categories
+      @categories = Category.all  
+    end
 end
